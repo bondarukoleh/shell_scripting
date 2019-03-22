c6a5ce3823a5fe429c02dc8551854492dd17f3e0
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-03-22 23:41:28 +0200

    function part 1 done

--- scripts/functions/functions.sh
+++ scripts/functions/functions.sh
@@ -11,3 +11,20 @@ function innerCall(){
 }
 
 echoArg "HAHA"
+
+PARAM_MESSAGE="Passed arg is - "
+
+function loopingArgs(){
+  local LOCAL_VAR='some local varible in funcionn'
+  for PARAM in $@
+  do
+    echo $PARAM_MESSAGE$PARAM
+  done
+}
+
+
+loopingArgs first_argument second_argument third_argument
+
+echo "Inner loopingArgs function variabe after function run is global now - $PARAM"
+echo "Inner loopingArgs function variabe - $LOCAL_VAR"
+
