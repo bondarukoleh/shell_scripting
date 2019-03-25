f02e43f6444d9002c9c14af4644ec38a26c2dc2f
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-03-25 18:45:01 +0200

    switch case continue

--- scripts/switchCase.sh
+++ scripts/switchCase.sh
@@ -1,1 +1,29 @@
 #!/usr/bin/env bash
+
+START=start
+STOP=stop
+
+function userInput(){
+  read -p "Input command: " COMMAND
+  action $COMMAND
+}
+
+
+function action(){
+  case $1 in
+    $START | START) #means OR in switch case.
+      echo "Started something"
+      userInput
+      ;;
+    $STOP | STOP)
+      echo "Stopped something" 
+      exit 0
+      ;;
+    *) # means cases above not fired - anything that user passed - brings here
+      echo "Pass 'start' or 'stop' please."
+      userInput
+      ;;
+  esac
+}
+
+action
