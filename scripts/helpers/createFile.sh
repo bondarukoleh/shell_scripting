fdf7ab5a9d559b9351eb6939ced88c37ba84ec62
Oleh Bondaruk <bondaruk9000@gmail.com>
2019-10-29 17:05:44 +0200

    added another method to white file

--- scripts/helpers/createFile.sh
+++ scripts/helpers/createFile.sh
@@ -2,7 +2,18 @@
 #Not working with git for windows, since it's not a trully shell.
 CREDENTIALS="User=Bob\nPass=1111";
 
-echo $CREDENTIALS > newFile.env;
+# echo $CREDENTIALS > newFile.env;
 
 # append
-echo $CREDENTIALS >> newFile.env;
+# echo $CREDENTIALS >> newFile.env;
+
+#another method, rewrites the file
+FILE="/D/Learn/shell_scripting/scripts/helpers/a.txt"
+cat <<EOM >$FILE
+text1
+text2 # This comment will be inside of the file.
+The keyword EOM can be any text, but it must start the line and be alone.
+ EOM # This will be also inside of the file, see the space in front of EOM.
+EOM # No comments and spaces around here, or it will not work.
+text4 
+EOM
